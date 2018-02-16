75665de7b63a9edb5b36c820ed7a99abee872d17
RBDOM\nanderson2 <Nicholas.Anderson@us.redbull.com>
2018-02-16 15:15:57 -0800

    simple array

--- js/index.js
+++ js/index.js
@@ -1,13 +1,17 @@
+const array = [1, 2, 3];
+
 var example =
   "Showing this string in the console! Because I'm a little biyatch and my name is Nick!";
 
-console.log(example);
+console.log(array[0], example);
 
 //Question for Nick
 var question =
   "How do I load a JS file into a browser so I can view the output of the JS code?";
 
-console.log(question);
+console.log(array[1], question);
 
-const anwser =
+const answer =
   "One of two ways. Either post your code in the .js file. *Best idea becuase that is what you'll be doing on the job.* or just copy and paste it into your browser console. Browsers can read javascript natively.";
+
+console.log(array[2], answer);
