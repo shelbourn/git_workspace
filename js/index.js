723ce4109f88915db44c2fbd728167670baf6bc5
RBDOM\nanderson2 <Nicholas.Anderson@us.redbull.com>
2018-02-16 15:20:08 -0800

    info on script tags

--- js/index.js
+++ js/index.js
@@ -10,7 +10,10 @@ var question =
 
 console.log(array[1], question);
 
-const answer =
-  "One of two ways. Either post your code in the .js file. *Best idea becuase that is what you'll be doing on the job.* or just copy and paste it into your browser console. Browsers can read javascript natively.";
+const answer = `One of two ways. Either post your code in the index.js file. 
+Or create another js file and referrance it in the index.html
+<script src="js/bitchFace.js"></script>
+*Best idea becuase that is what you'll be doing on the job.*
+Or just copy and paste it into your browser console. Browsers can read javascript natively.`;
 
 console.log(array[2], answer);
