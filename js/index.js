6f9254b9c9374957da22b95aacb2726e31d1d0b7
RBDOM\nanderson2 <Nicholas.Anderson@us.redbull.com>
2018-02-16 13:26:20 -0800

    .

--- js/index.js
+++ js/index.js
@@ -1,8 +1,13 @@
-var example = "Showing this string in the console! Because I'm a little biyatch and my name is Nick!";
+var example =
+  "Showing this string in the console! Because I'm a little biyatch and my name is Nick!";
 
 console.log(example);
 
 //Question for Nick
-var question = "How do I load a JS file into a browser so I can view the output of the JS code?";
+var question =
+  "How do I load a JS file into a browser so I can view the output of the JS code?";
 
 console.log(question);
+
+const anwser =
+  "One of two ways. Either post your code in here. *Best idea becuase that is what you'll be doing.* or just copy and paste it into your browser console.";
