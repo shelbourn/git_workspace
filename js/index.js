c4d987d97f43e11c49546a27633a2073dd7bf888
shelbourn <mattshelbourn@gmail.com>
2018-02-16 11:52:30 -0800

    Adding a question var with a new console output to the JS file.

--- js/index.js
+++ js/index.js
@@ -1,3 +1,7 @@
 var example = "Showing this string in the console! Because I'm a little biyatch and my name is Nick!";
 
 console.log(example);
+
+var question = "How do I load a JS file into a browser so I can view the output of the JS code?";
+
+console.log(question);
