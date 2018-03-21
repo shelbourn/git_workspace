3b6e4128cc5d2eb356a8298114a808a5f3c1ca40
NicKAnderson038 <nickanderson@gmail.com>
2018-03-20 22:26:40 -0700

    config file test features and updated package.json

--- server/config/config-p.js
+++ server/config/config-p.js
@@ -1,3 +1,10 @@
-const example = "Production";
+const example = "production";
+process.env.NODE_ENV = example;
 
 console.log(`${example} configuration`);
+
+console.log({ myVar: process.env.yarn_config_myVar });
+
+process.argv.forEach((val, index) => {
+  console.log(`${index}: ${val}`);
+});
